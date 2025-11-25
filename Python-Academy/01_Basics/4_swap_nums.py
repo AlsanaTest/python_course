400b762df0345ee1bf80c6a5884adec02a82d4a6
Alsana Rangrej <alsanarangrej@gmail>
2025-11-25 19:42:56 +0530

    Solved

--- Python-Academy/01_Basics/4_swap_nums.py
+++ Python-Academy/01_Basics/4_swap_nums.py
@@ -1,7 +1,18 @@
+
 # Take two numbers as input
+A = input("Enter the first number: ")
+B = input("Enter the second number: ")
+
+# Print before swapping
+print("Before swapping:", A, B)
 
 # Store one value in temp
+temp = A
 
 # Swap the variables
+A = B
+B = temp
 
-# Print before and after swapping
+# Print after swapping
+print("After swapping:", A, B)
+

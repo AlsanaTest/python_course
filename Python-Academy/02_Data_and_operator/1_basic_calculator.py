c735c4db50142e55eeeddd22826ccedbe5ffb9a4
Alsana Rangrej <alsanarangrej@gmail>
2025-11-25 20:16:32 +0530

    Solved

--- Python-Academy/02_Data_and_operator/1_basic_calculator.py
+++ Python-Academy/02_Data_and_operator/1_basic_calculator.py
@@ -1,8 +1,18 @@
 # Take two numbers as input
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
 
 # Print sum
+print("Sum:", A + B)
 
 # Print difference
+print("Difference:", A - B)
+
 # Print product
+print("Product:", A * B)
+
 # Print quotient
+print("Quotient:", A / B)
+
 # Print remainder
+print("Remainder:", A % B)

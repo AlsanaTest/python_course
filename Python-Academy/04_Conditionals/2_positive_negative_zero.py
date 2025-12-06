d17730035493e0a1f8dfa3c03889aa6b981729ac
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 12:31:29 +0530

    Solved

--- Python-Academy/04_Conditionals/2_positive_negative_zero.py
+++ Python-Academy/04_Conditionals/2_positive_negative_zero.py
@@ -1,3 +1,13 @@
 # Input a number
+n = int(input("Enter a number: "))
+
 # Check if positive, negative, or zero
+if n > 0:
+    result = "Positive"
+elif n < 0:
+    result = "Negative"
+else:
+    result = "Zero"
+
 # Print the result
+print("The number is:", result)

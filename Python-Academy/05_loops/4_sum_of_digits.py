9acfc9a451b0d61b750b327c74f59950cafafb7a
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 11:56:08 +0530

    Solved

--- Python-Academy/05_loops/4_sum_of_digits.py
+++ Python-Academy/05_loops/4_sum_of_digits.py
@@ -1,5 +1,20 @@
 # Input a number
+n = int(input("Enter a number: "))
+
 # Use loop to extract digits (using % and //)
+sum_digits = 0
+product_digits = 1
+
 # Maintain sum of digits
+temp = n
+
 # Maintain product of digits
+while temp > 0:
+    digit = temp % 10        # Extract last digit
+    sum_digits += digit      # Add to sum
+    product_digits *= digit  # Multiply to product
+    temp = temp // 10        # Remove last digit
+
 # Print both values
+print("Sum of digits:", sum_digits)
+print("Product of digits:", product_digits)

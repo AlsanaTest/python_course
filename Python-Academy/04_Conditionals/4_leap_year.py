526c3554e512363c47a0557f214859892eebfa1e
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 12:36:03 +0530

    Solved

--- Python-Academy/04_Conditionals/4_leap_year.py
+++ Python-Academy/04_Conditionals/4_leap_year.py
@@ -1,3 +1,11 @@
 # Input a year
+year = int(input("Enter a year: "))
+
 # Check if divisible by 4
+if year % 4 == 0:
+    result = "Leap year"
+else:
+    result = "Not a leap year"
+
 # Print if leap year or not
+print(result)

92e2b2a47ace7e300ae01af37099c3e95e21b2eb
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 12:34:06 +0530

    Solved

--- Python-Academy/04_Conditionals/3_calc_grade.py
+++ Python-Academy/04_Conditionals/3_calc_grade.py
@@ -1,3 +1,17 @@
 # Input marks from 0 to 100
+marks = int(input("Enter marks: "))
+
 # Use conditions to assign grade
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
+elif marks >= 35:
+    grade = "D"
+else:
+    grade = "Fail"
+
 # Print the grade
+print("Grade:", grade)

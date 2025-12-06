f0ea18a637406044ce9b034335a9400b658244de
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 12:16:23 +0530

    Solved

--- Python-Academy/05_loops/5_reverse_a_number.py
+++ Python-Academy/05_loops/5_reverse_a_number.py
@@ -1,3 +1,14 @@
 # Input a number
+n = int(input("Enter a number: "))
+
 # Use loop to reverse the digits of the number
+reverse_num = 0
+temp = n
+
+while temp > 0:
+    digit = temp % 10
+    reverse_num = reverse_num * 10 + digit
+    temp = temp // 10
+
 # Print the reversed number
+print("Reversed number:", reverse_num)

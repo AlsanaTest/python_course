40f6d776ea01464fbbe5b8cf4189ec044c10501b
Alsana Rangrej <alsanarangrej@gmail>
2025-12-06 12:29:53 +0530

    Solved

--- Python-Academy/04_Conditionals/1_greater_of_two.py
+++ Python-Academy/04_Conditionals/1_greater_of_two.py
@@ -1,3 +1,12 @@
 # Input two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
 # Use if-else to find greater
+if a > b:
+    greater = a
+else:
+    greater = b
+
 # Print the greater number
+print("Greater number is:", greater)
